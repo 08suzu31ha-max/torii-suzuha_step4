fb42e181ae34626ad51d2fd699db24f88fca61bb
08suzu31ha-max <08suzu31ha@gmail.com>
2026-09-24 10:16:10 +0900

    初回コミット

---
+++ src/confirm.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <title>入力内容確認</title>
+</head>
+<body>
+
+<h1>入力内容確認</h1>
+<?php
+ if($_SERVER["REQUEST_METHOD"]==="POST"){
+    $username=$_POST["username"];
+    $age=$_POST["age"];
+    $phonenumber=$_POST["phone"];
+    $email=$_POST["email"];
+    $address=$_POST["address"];
+    $question=$_POST["question"];
+    $gender=$_POST["gender"];
+
+if (!preg_match("/^[ぁ-んァ-ヶ一-龯a-zA-Z]+$/u", $username)) {
+    echo "名前はひらがな、カタカナ、漢字、英字のみ使用できます。";
+    exit;
+}
+if ($age < 0 || $age > 150) {
+    echo "年齢は0〜150の間で入力してください。";
+    exit;
+}
+if(!preg_match("/^[0-9-]+$/",$phonenumber)){
+    echo "電話番号は半角数字とハイフンのみ使用できます。";
+    exit;
+}
+if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+    echo"メールアドレスの形式が正しくありません。";
+    exit;
+ }
+if(!preg_match("/^[ぁ-んァ-ヶー-龯a-zA-Z0-9-]+$/u" , $address)){
+    echo"住所はひらがな、カタカナ、漢字、英字、半角数字、ハイフンのみ使用できます。";
+    exit;
+}
+
+else{
+    echo"名前：" . $username . "<br>";
+    echo"年齢：" . $age . "<br>";
+    echo"電話番号：" . $phonenumber . "<br>";
+    echo"メールアドレス：" . $email . "<br>";
+    echo"住所：" . $address . "<br>";
+    echo"質問：" . $question . "<br>";
+    echo"性別：" . $gender . "<br>";
+}
+ }
